ba03e40ead3dc9f8a24115c06a6a868cfbc58708
Aiman <aimanusmani1700@gmail.com>
2024-05-28 16:40:25 +0530

    Java programs

---
+++ Programming/OOPs/p1.java
@@ -0,0 +1,14 @@
+class areaCalculator{
+
+public void area(int l,int b){
+return l*b;
+}
+public void area( int radius){
+return 2.71*radius*radius;
+}
+}
+
+public static void main(String[] args){
+        System.ot.println("Area is:  "+area(6,9));
+        System.ot.println("Area is:  "+area(7));
+}
---
+++ Programming/OOPs/p2.java
@@ -0,0 +1,7 @@
+abstract class Animal{
+    public abstract void sound();
+
+    public void sleep(){
+        System.out.println("This animal is sleeping");
+    }
+}
