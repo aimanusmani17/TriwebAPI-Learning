fe23c28166a5c2f0e33b1ee95cc43c379c1e6f79
Aiman <aimanusmani1700@gmail.com>
2024-06-01 17:07:56 +0530

    added abstraction,inheritance and polymorphism

--- Programming/OOPs/p2.java
+++ Programming/OOPs/p2.java
@@ -5,3 +5,39 @@ abstract class Animal{
         System.out.println("This animal is sleeping");
     }
 }
+// dog class extends animal
+ class Dog extends Animal{
+
+    //override
+    public void sound(){
+        System.out.println("The dog barks.");
+    }
+
+ } 
+ class Cat extends Animal{
+    //override
+    public void sound(){
+        System.out.println("The cat meows.");
+    }
+}
+
+public class Main{
+    
+    //method to demonstrate polymorphism
+    public static void animalSound(Animal animal ){
+        animal.sound();
+    }
+    
+public static void main(String[] args){
+
+    Animal myDog= new Dog();
+    Animal myCat= new Cat();
+
+    myDog.sound();
+    myCat.sound();
+
+    myDog.sleep();
+    myCat.sleep();
+
+}
+}
