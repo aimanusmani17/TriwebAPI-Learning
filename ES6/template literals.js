38b14ff2a4886d6626893d216b8aebae6136e139
aimanusmani1700@gmail.com <aimanusmani@gmail.com>
2024-06-11 21:43:26 +0530

    Add Template literals program

---
+++ ES6/template literals.js
@@ -0,0 +1,12 @@
+// Template literals
+
+
+function printDate(sname, age){
+    let toPrint= "student's anme is " +sname + "and age is: " +age;
+    console.log(printDate);
+}
+
+let sname="aiman";
+let age= 28;
+
+printDate(sname,age);
