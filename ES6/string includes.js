ebeb86a1255804cd96d693df9c3e6984346c1d62
aimanusmani1700@gmail.com <aimanusmani@gmail.com>
2024-06-12 15:51:29 +0530

    Added String includes

---
+++ ES6/string includes.js
@@ -0,0 +1,9 @@
+// . includes()
+
+let str="This is a good city";
+
+// console.log(str.includes("city")); //true
+
+console.log(str.startsWith("that")); //false
+
+console.log(str.endsWith("city"));
