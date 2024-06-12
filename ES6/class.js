ba5ed40153b2fc42011c8ede79dc6e76be57f77b
aimanusmani1700@gmail.com <aimanusmani@gmail.com>
2024-06-12 12:18:19 +0530

    added class program

---
+++ ES6/class.js
@@ -0,0 +1,12 @@
+class Student{
+
+    constructor(sname,roll_no){
+        this.name= sname;
+        this.roll_no= roll_no;
+
+    }
+}
+
+const student1= new Student("aiman", 6);
+
+console.log(student1)
