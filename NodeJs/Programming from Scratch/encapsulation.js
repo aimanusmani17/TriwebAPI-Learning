866f8db7fc9f2421c1d996f0a776b9b5b77fe4dd
aimanusmani1700@gmail.com <aimanusmani@gmail.com>
2024-06-27 01:12:32 +0530

    Encapsulation program

---
+++ NodeJs/Programming from Scratch/encapsulation.js
@@ -0,0 +1,34 @@
+class BankAccount{
+    constructor(accNumber,accHolderName,balance){
+        this.accNumber= accNumber;
+        this.accHolderName=accHolderName;
+        this.balance=balance;
+    }
+
+    showAccDetails(){
+        console.log(`Account Number: ${this.accNumber}`);
+        console.log(`Account Holder Name is: ${this.accHolderName}`);
+        console.log(`Balance is: ${this.balance}`);
+
+    }
+
+    deposit(amount){
+        this.balance += amount;
+        this.showAccDetails();
+
+    }
+
+    withdraw(amount){
+        if (this.balance >=amount){
+            this.balance -= amount;
+            this.showAccDetails();
+
+        }
+        else{
+            console.log("Insufficient Balance");
+        }
+    }
+}
+
+     let myBankAcc= new BankAccount("3456", "Akshi", "5000");
+     myBankAcc.deposit(1000);
