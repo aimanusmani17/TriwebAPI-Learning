e69046f40fec49ba96793b68e3d1c63cd84d92d8
aimanusmani1700@gmail.com <aimanusmani@gmail.com>
2024-06-27 00:49:22 +0530

    Inheritance Program

---
+++ NodeJs/Programming from Scratch/inheritance.js
@@ -0,0 +1,28 @@
+// To create a class inheritance, use the extends keyword.
+
+// parent class
+class Person { 
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+// inheriting parent class
+class Student extends Person {
+
+    constructor(name) {
+    
+        console.log("Creating student class");
+        
+        // call the super class constructor and pass in the name parameter
+        super(name);
+    }
+
+}
+
+let student1 = new Student('Jack');
+student1.greet();
