b63585c1e1b9cbdf031a15d84754e648324d7806
aimanusmani1700@gmail.com <aimanusmani@gmail.com>
2024-06-27 00:15:48 +0530

    program to print natural numbers

---
+++ NodeJs/Programming from Scratch/natural numbers.js
@@ -0,0 +1,17 @@
+// using for loop
+for(let i=1; i<=10 ; i++){
+    console.log(i);
+}
+
+// using while loop
+ let x=1, z=10
+ while(x<=10){
+    console.log(x);
+    x++;
+ }
+
+//  continue statment
+for (let p = 0; p < 10; p++) {
+    if (p === 3) continue;
+    console.log(p);
+}
