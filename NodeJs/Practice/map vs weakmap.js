fdc31c48b566155f006735e5d7dad934e8053ed0
aimanusmani1700@gmail.com <aimanusmani@gmail.com>
2024-07-04 17:56:18 +0530

    map and weakmap

---
+++ NodeJs/Practice/map vs weakmap.js
@@ -0,0 +1,16 @@
+// map and weakmap
+
+let user={
+    uname:"Aiman",
+    mobile:989877
+}
+
+let key1={};
+let key2={uname:"Aiman"};
+let weakMApExample={
+
+    [key1]: "Some data",
+    [key2]:"new data"
+}
+
+console.log(weakMApExample);
