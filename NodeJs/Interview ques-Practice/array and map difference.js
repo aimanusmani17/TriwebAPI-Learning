a4de2d8266b16bea58542ca5eac0175c345cd476
aimanusmani1700@gmail.com <aimanusmani@gmail.com>
2024-07-05 17:29:48 +0530

    ordered folder

---
+++ NodeJs/Interview ques-Practice/array and map difference.js
@@ -0,0 +1,18 @@
+/*An array is a collection of itmes stored at contigious memory location
+-->stores multiple items of same type
+
+*/
+
+let arr=[1,2,3,4,5,6,7,8,9];
+
+/* 
+A map is an associative container that stores
+elements in a mapped manner.
+
+--> use to store records {example person record of different data type)}
+*/
+
+let mapE= {
+    uname:"aiman",
+    mobile: 98789,
+}
